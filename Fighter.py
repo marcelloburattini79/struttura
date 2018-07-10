9c6f9a0a91e6b4d4342e5257f3c1c445cd276874
marcelloburattini79 <burattinimarcello@yahoo.it>
2018-07-10 09:03:02 +0200

    Initial commit

---
+++ Fighter.py
@@ -0,0 +1,21 @@
+class Fighter:
+
+    def __init__(self, nome, eta, hp, attacco, difesa):
+
+        self.nome = nome
+
+        self.eta = eta
+
+        self.hp = hp
+
+        self.attacco = attacco
+
+        self.difesa = difesa
+
+    def __str__(self):
+
+        return self.nome
+
+
+
+
---
+++ main.py
@@ -0,0 +1,9 @@
+from Fighter import Fighter
+
+ryu = Fighter('Ryu', 35, 40, 16, 12)
+
+blanka = Fighter('Blanka', 42, 50, 14, 11)
+
+print(ryu)
+
+print(blanka)
